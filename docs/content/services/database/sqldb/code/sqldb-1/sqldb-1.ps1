4607b023ce3af3b82d2ec9b6892d7835012f0412
Dost Muhammad <dostmuh@microsoft.com>
2023-10-10 14:48:15 -0600

    Sql DB graph/powershell queries

---
+++ docs/content/services/database/sqldb/code/sqldb-1/sqldb-1.ps1
@@ -0,0 +1,11 @@
+##Update-AzConfig -DisplayBreakingChangeWarning $false
+$servers = Get-AzSqlServer
+##Loop through all servers
+foreach($server in $servers) {
+    $databases= Get-AzSqlDatabase -ServerName $server.ServerName -ResourceGroupName $server.ResourceGroupName
+    foreach($db in $databases) {
+        if ($db.CurrentBackupStorageRedundancy -eq 'Local' -or $db.CurrentBackupStorageRedundancy -eq 'Zone') {
+            Write-Host "recommendationId=SQLDB-1 SQLServerName=" $server.ServerName "databasename=" $db.DatabaseName  "Redundancy="  $db.CurrentBackupStorageRedundancy
+        }
+    }
+}
