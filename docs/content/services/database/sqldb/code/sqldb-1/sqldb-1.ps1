6f176663d5f7dc601631907bf017804f44254e65
Dost Muhammad <dostmuh@microsoft.com>
2023-10-23 14:34:07 -0600

    fixing powershell to output display format as requested

--- docs/content/services/database/sqldb/code/sqldb-1/sqldb-1.ps1
+++ docs/content/services/database/sqldb/code/sqldb-1/sqldb-1.ps1
@@ -1,11 +1,11 @@
-##Update-AzConfig -DisplayBreakingChangeWarning $false
+
 $servers = Get-AzSqlServer
 ##Loop through all servers
 foreach($server in $servers) {
     $databases= Get-AzSqlDatabase -ServerName $server.ServerName -ResourceGroupName $server.ResourceGroupName
     foreach($db in $databases) {
-        if ($db.CurrentBackupStorageRedundancy -eq 'Local' -or $db.CurrentBackupStorageRedundancy -eq 'Zone') {
-            Write-Output( "recommendationId=SQLDB-1;SQLServerName=" +$server.ServerName +";databasename=" +$db.DatabaseName + ";Redundancy=" + $db.CurrentBackupStorageRedundancy)
+        if ($db.CurrentBackupStorageRedundancy -eq 'Local' -or $db.CurrentBackupStorageRedundancy -ne 'Zone') {
+            Write-Output( "recommendationId=sqldb-1, databasename=" +$db.DatabaseName + ",id=" + $db.DatabaseId)
         }
     }
 }
