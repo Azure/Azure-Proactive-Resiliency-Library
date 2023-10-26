85bec40497c8fe491692a128aeeba33beb9affde
Dost Muhammad <dostmuh@microsoft.com>
2023-10-26 10:46:59 -0600

    Fixing Query Parameters

--- docs/content/services/database/sqldb/code/sqldb-1/sqldb-1.ps1
+++ docs/content/services/database/sqldb/code/sqldb-1/sqldb-1.ps1
@@ -5,7 +5,7 @@ foreach($server in $servers) {
     $databases= Get-AzSqlDatabase -ServerName $server.ServerName -ResourceGroupName $server.ResourceGroupName
     foreach($db in $databases) {
         if ($db.CurrentBackupStorageRedundancy -eq 'Local' -or $db.CurrentBackupStorageRedundancy -ne 'Zone') {
-            Write-Output( "recommendationId=sqldb-1, databasename=" +$db.DatabaseName + ",id=" + $db.DatabaseId)
+            Write-Output( "recommendationId=sqldb-1, name=" +$db.DatabaseName + ",id=" + $db.DatabaseId)
         }
     }
 }
